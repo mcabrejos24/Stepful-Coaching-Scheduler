27d9fe8a7776931c3ac2d10a1c2cb1fb8238a175
mcabrejos24 <manuelcabrejos97@gmail.com>
2023-10-16 09:34:37 -0400

    update schemas

--- prisma/migrations/20231016133342_initial_models/migration.sql
+++ prisma/migrations/20231016133342_initial_models/migration.sql
@@ -33,8 +33,22 @@ CREATE TABLE "TimeSlot" (
     CONSTRAINT "TimeSlot_bookedBy_fkey" FOREIGN KEY ("bookedBy") REFERENCES "Student" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "MeetingNotes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "timeSlotId" INTEGER NOT NULL,
+    "notes" TEXT NOT NULL,
+    "satisfaction" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "MeetingNotes_timeSlotId_fkey" FOREIGN KEY ("timeSlotId") REFERENCES "TimeSlot" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Coach_email_key" ON "Coach"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MeetingNotes_timeSlotId_key" ON "MeetingNotes"("timeSlotId");
