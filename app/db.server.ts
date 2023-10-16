b16913e1d13f6b343be8f8a69de962ac71c2ba8d
mcabrejos24 <manuelcabrejos97@gmail.com>
2023-10-16 04:53:55 -0400

    Setup and configuration (#1)
    * install prisma with sqlite
    
    * install react-big-calendar and moment
    
    * install and setup prisma client
    
    * add material UI and set up theme
    
    * set up react big calendar

---
+++ app/db.server.ts
@@ -0,0 +1,6 @@
+// in some file like db.js
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,75 @@
 import type { MetaFunction } from "@remix-run/node";
+import theme from "../theme";
+import {
+  ThemeProvider,
+  Button,
+  Container,
+  Typography,
+  Paper,
+  AppBar,
+  Toolbar,
+} from "@mui/material";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+
+const localizer = momentLocalizer(moment);
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Stepful Coaching Scheduler" },
+    { name: "description", content: "Coaching Scheduler" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Stepful Coaching Scheduler</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md">
+        <Paper elevation={3} style={{ padding: "2rem", margin: "2rem 0" }}>
+          <Typography variant="h3" color="primary" gutterBottom>
+            Calendar
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Welcome to the Stepful Coaching Scheduler. Use this platform to
+            manage your coaching sessions efficiently.
+          </Typography>
+          <Button variant="contained" color="primary">
+            Schedule a Session
+          </Button>
+          <Button
+            variant="outlined"
+            color="secondary"
+            style={{ marginLeft: "1rem" }}
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+            Learn More
+          </Button>
+        </Paper>
+        <Calendar
+          localizer={localizer}
+          events={[
+            {
+              title: "My Event",
+              start: new Date(),
+              end: new Date().setHours(new Date().getHours() + 2),
+            },
+            {
+              title: "My Event",
+              start: new Date(new Date().getTime() + 15 * 60 * 1000),
+              end: new Date(
+                new Date().getTime() + 15 * 60 * 1000 + 2 * 60 * 60 * 1000
+              ),
+            },
+          ]}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </Container>
+    </ThemeProvider>
   );
 }
