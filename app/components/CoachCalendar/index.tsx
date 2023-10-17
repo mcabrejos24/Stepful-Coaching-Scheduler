38970c2e0f2a9e7b457e3a8522b154654e277c5d
mcabrejos24 <manuelcabrejos97@gmail.com>
2023-10-17 03:51:07 -0400

    create coach calendar

---
+++ app/components/CoachCalendar/index.tsx
@@ -0,0 +1,59 @@
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import { Calendar, type Event, momentLocalizer } from "react-big-calendar";
+import { Container, useTheme } from "@mui/material";
+
+const localizer = momentLocalizer(moment);
+
+interface CoachCalendarProps {
+  events?: Event[];
+}
+
+export function CoachCalendar(props: CoachCalendarProps) {
+  const theme = useTheme();
+  const { events } = props;
+
+  const customEventPropGetter = (
+    event: Event,
+    start: Date,
+    end: Date,
+    isSelected: boolean
+  ) => {
+    let backgroundColor = theme.palette.info.main; // default color
+    if (event.resource.hasConflict) {
+      backgroundColor = theme.palette.error.main;
+    } else {
+      switch (event.resource.status) {
+        case "booked":
+          backgroundColor = theme.palette.secondary.main;
+          break;
+        case "incomplete":
+          backgroundColor = theme.palette.warning.main;
+          break;
+        case "complete":
+          backgroundColor = theme.palette.success.main;
+        default:
+          backgroundColor = theme.palette.info.main;
+          break;
+      }
+    }
+
+    return { style: { backgroundColor } };
+  };
+
+  return (
+    <Container
+      maxWidth="md"
+      style={{ marginTop: "2rem", height: "75vh", overflowY: "auto" }}
+    >
+      <Calendar
+        localizer={localizer}
+        defaultView="week"
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        eventPropGetter={customEventPropGetter}
+      />
+    </Container>
+  );
+}
