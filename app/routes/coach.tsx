f5f224287162cea7e7b72d82af82aa49ad8f04b4
mcabrejos24 <manuelcabrejos97@gmail.com>
2023-10-17 22:16:19 -0400

    add student page and update coach page

--- app/routes/coach.tsx
+++ app/routes/coach.tsx
@@ -1,10 +1,12 @@
+import { type MeetingNotes, type Student } from "@prisma/client";
 import { type MetaFunction } from "@remix-run/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { type Event } from "react-big-calendar";
 import { ActionBar } from "~/components/ActionBar";
 import { AddAvailabilityModal } from "~/components/AddAvailabilityModal";
 import { CoachCalendar } from "~/components/CoachCalendar";
 import { RoleSwitchFooter } from "~/components/RoleSwitchFooter";
+import { TimeSlotModal } from "~/components/TimeSlotModal";
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,28 +15,92 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-interface HydratedTimeSlot {
+export interface HydratedTimeSlot {
+  id: number;
   duration: number;
   startTime: Date;
   status: string;
-  bookedBy?: string;
+  bookedBy?: Partial<Student>;
   hasConflict?: boolean;
+  meetingNotes?: Partial<MeetingNotes>;
 }
 
 export default function Coach() {
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
+  const [isSlotModalOpen, setSlotModalOpen] = useState(false);
+  const [selectedSlot, setSelectedSlot] = useState<Event | null>(null);
+  const [events, setEvents] = useState<Event[]>([]);
   const [timeSlots, setTimeSlots] = useState<HydratedTimeSlot[]>([
     {
+      id: 1,
       duration: 2,
       startTime: new Date(new Date().setSeconds(0, 0)),
       status: "booked",
-      bookedBy: "Neo",
+      bookedBy: {
+        id: 1,
+        name: "Neo",
+      },
     },
     {
+      id: 2,
       duration: 2,
       startTime: new Date(new Date().setSeconds(0, 0)),
       status: "available",
       hasConflict: true,
+    },
+    {
+      id: 3,
+      duration: 2,
+      startTime: new Date(
+        new Date(new Date().setDate(new Date().getDate() + 1)).setHours(
+          10,
+          0,
+          0,
+          0
+        )
+      ),
+      status: "available",
+      hasConflict: false,
+    },
+    {
+      id: 4,
+      duration: 2,
+      startTime: new Date(
+        new Date(new Date().setDate(new Date().getDate() - 1)).setHours(
+          2,
+          0,
+          0,
+          0
+        )
+      ),
+      status: "incomplete",
+      hasConflict: false,
+      bookedBy: {
+        id: 1,
+        name: "Neo",
+      },
+    },
+    {
+      id: 5,
+      duration: 2,
+      startTime: new Date(
+        new Date(new Date().setDate(new Date().getDate() - 1)).setHours(
+          6,
+          0,
+          0,
+          0
+        )
+      ),
+      status: "complete",
+      hasConflict: false,
+      bookedBy: {
+        id: 1,
+        name: "Neo",
+      },
+      meetingNotes: {
+        notes: "Good meetings. Talked a lot.",
+        satisfaction: "1",
+      },
     },
   ]);
 
@@ -47,11 +113,14 @@ export default function Coach() {
     // save to setTimeSlots
     // should rerender calendar
     const dateObject = new Date(`${selectedDate}T${selectedTime}:00`);
-
+    const status = dateObject < new Date() ? "incomplete" : "availabile";
+    // TODO: if filling out for a previous date, include logic to add the student they met with
+    // TODO: possibly an option to fill out feedback here as well
     const newSlot: HydratedTimeSlot = {
+      id: 3,
       duration: 2,
       startTime: dateObject,
-      status: "available",
+      status: status,
     };
 
     const hasConflict = timeSlots.some((slot) => {
@@ -79,23 +148,53 @@ export default function Coach() {
     setTimeSlots([...timeSlots, newSlot]);
   };
 
-  const events: Event[] = timeSlots.map((slot) => {
-    const start = new Date(slot.startTime);
-    const end = new Date(start.getTime() + slot.duration * 60 * 60 * 1000);
-    return {
-      title: slot.bookedBy
-        ? `Booked by ${slot.bookedBy}`
-        : slot.hasConflict
-        ? "Has conflict"
-        : "Available",
-      start,
-      end,
-      resource: {
-        status: slot.status,
-        hasConflict: slot.hasConflict,
-      },
-    };
-  });
+  const handleSlotClick = (event: Event) => {
+    setSelectedSlot(event);
+    setSlotModalOpen(true);
+  };
+
+  const handleTimeSlotUpdate = (
+    id: number,
+    notes?: string,
+    satisfaction?: string,
+    studentBooking?: boolean
+  ) => {
+    // hit backend to update the id
+    // refetch the timeslots via an endpoint
+    // save new timeslots to savedState - for now just find id and update slot
+    const newSlots = timeSlots.map((slot) => {
+      if (slot.id === id)
+        return { ...slot, meetingNotes: { notes, satisfaction } };
+      return slot;
+    });
+    setTimeSlots(newSlots);
+  };
+
+  useEffect(() => {
+    const eventsArr: Event[] = timeSlots.map((slot) => {
+      const start = new Date(slot.startTime);
+      const end = new Date(start.getTime() + slot.duration * 60 * 60 * 1000);
+      return {
+        title: slot.bookedBy
+          ? `Booked by ${slot.bookedBy.name}`
+          : slot.hasConflict
+          ? "Has conflict"
+          : "Available",
+        start,
+        end,
+        resource: {
+          id: slot.id,
+          status: slot.status,
+          ...(slot?.hasConflict && { hasConflict: slot.hasConflict }),
+          ...(slot?.meetingNotes?.notes && { notes: slot.meetingNotes.notes }),
+          ...(slot?.meetingNotes?.satisfaction && {
+            satisfaction: slot.meetingNotes.satisfaction,
+          }),
+        },
+      };
+    });
+    setEvents(eventsArr);
+  }, [timeSlots]);
 
   return (
     <>
@@ -105,7 +204,19 @@ export default function Coach() {
         onClose={() => setModalOpen(false)}
         onAddAvailability={handleAddAvailability}
       />
-      <CoachCalendar events={events} />
+      <CoachCalendar events={events} handleSlotClick={handleSlotClick} />
+      {selectedSlot && (
+        <TimeSlotModal
+          isOpen={isSlotModalOpen}
+          onClose={() => {
+            setSlotModalOpen(!isSlotModalOpen);
+            setSelectedSlot(null);
+          }}
+          userType="coach"
+          event={selectedSlot}
+          updateTimeSlot={handleTimeSlotUpdate}
+        />
+      )}
       <RoleSwitchFooter userRole={"coach"} />
     </>
   );
